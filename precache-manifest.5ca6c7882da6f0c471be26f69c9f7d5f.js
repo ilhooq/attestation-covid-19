07f103b59570fe7ca1f09204b77160e513814a7e
ilhooq <contact@sphilip.com>
2020-04-16 17:17:38 +0200

    deploy

---
+++ precache-manifest.5ca6c7882da6f0c471be26f69c9f7d5f.js
@@ -0,0 +1,37 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/attestation-covid-19/343ac8bb3542a6578f2a4cbaf0f99679.pdf"
+  },
+  {
+    "revision": "570c30e14ee9bc9d74ea",
+    "url": "/attestation-covid-19/css/app.31538c96.css"
+  },
+  {
+    "revision": "987220499d58e6d9f4daf9f7dbc45048",
+    "url": "/attestation-covid-19/index.html"
+  },
+  {
+    "revision": "2a11fbb297cb46d2dd17",
+    "url": "/attestation-covid-19/js/about.a303cb89.js"
+  },
+  {
+    "revision": "570c30e14ee9bc9d74ea",
+    "url": "/attestation-covid-19/js/app.f1459e63.js"
+  },
+  {
+    "revision": "63590b52e0114b95b9b0",
+    "url": "/attestation-covid-19/js/chunk-2d0c8225.82fa0aa1.js"
+  },
+  {
+    "revision": "618555c4b6f5544eb63d",
+    "url": "/attestation-covid-19/js/chunk-vendors.17e07ddc.js"
+  },
+  {
+    "revision": "eb6e8bd99d92fdcc28918783742994d7",
+    "url": "/attestation-covid-19/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/attestation-covid-19/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/attestation-covid-19/precache-manifest.5ca6c7882da6f0c471be26f69c9f7d5f.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "attestation-deplacement"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
